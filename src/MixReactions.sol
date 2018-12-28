b53d54314c9e1d749545014eb23d7b6fe4b09296
Jonathan Brown <jbrown@mix-blockchain.org>
2018-12-29 00:02:10 +0700

    Add mapping.

--- src/MixReactions.sol
+++ src/MixReactions.sol
@@ -1,4 +1,11 @@
 pragma solidity ^0.5.0;
 
+
 contract MixReactions {
+
+    /**
+     * @dev Mapping of account to mapping of itemId to mapping of reaction block to packed reactions.
+     */
+    mapping (address => mapping(bytes32 => mapping(uint => bytes32))) accountItemBlockReactions;
+
 }
